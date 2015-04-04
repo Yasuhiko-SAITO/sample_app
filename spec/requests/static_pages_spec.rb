87b7b4319d450171b299fb0ce236a54c72056d6c
Yasuhiko-SAITO <yasuhiko.saito.ab.c@gmail.com>
2015-04-04 15:14:43 +0900

    add About Page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,4 +18,12 @@ describe "StaticPages" do
     end
   end
 
+  describe "About pages" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
 end
