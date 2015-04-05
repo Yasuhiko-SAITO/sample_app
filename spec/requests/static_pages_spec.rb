46914a40d385a4080780dfb4d2ad96c6875ca246
Yasuhiko-SAITO <yasuhiko.saito.ab.c@gmail.com>
2015-04-06 00:10:48 +0900

    do practice c5-1 and c5-2.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,35 +6,73 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home pages" do
   	before { visit root_path }
+  	let(:heading) { 'Sample App' }
+  	let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+  	it_should_behave_like "all static pages"
     it { should_not have_title(' | Home') }
   end
 
 
   describe "Help pages" do
   	before {visit help_path}
+  	let(:heading) { 'Help' }
+  	let(:page_title) { '' }
 
-    it {should have_content('Help') }
-    it {should have_title(full_title('Help')) }
+  	it_should_behave_like "all static pages"
   end
 
 
   describe "About pages" do
   	before {visit about_path}
+  	let(:heading) { 'About Us' }
+  	let(:page_title) { '' }
 
-    it {should have_content('About') }
-    it {should have_title(full_title('About Us')) }
+  	it_should_behave_like "all static pages"
+
   end
 
 
   describe "Contact pages" do
   	before {visit contact_path}
+  	let(:heading) { 'Contact' }
+  	let(:page_title) { '' }
 
-    it {should have_content('Contact') }
-    it {should have_title(full_title('Contact')) }
+  	it_should_behave_like "all static pages"
+
+  end
+
+  it "should have the right links on the layout" do
+  	visit root_path
+  	click_link "About"
+   	expect(page).to have_title(full_title('About Us'))
+  	click_link "Contact"
+  	expect(page).to have_title(full_title('Contact'))
+
+
+#何故かambigiousエラー
+#  	click_link "Help"
+#   expect(page).to have_title(full_title('Help'))
+#  	click_link "Sample app"
+#  	expect(page).to have_title(full_title('Home'))
+
+#これだとFailure/Error
+#   within(first('header')) do
+#     click_link "Help"
+#  	  expect(page).to have_title(full_title('Help'))
+#  	end
+
+
+    visit root_path
+  	click_link "Sign up now!"
+  	expect(page).to have_title(full_title('Sign up'))
+
   end
 end
