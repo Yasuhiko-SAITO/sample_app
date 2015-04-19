db1788da8be8ca5694e064ea2e8711c531301315
Yasuhiko-SAITO <yasuhiko.saito.ab.c@gmail.com>
2015-04-19 20:40:10 +0900

    chap 9-2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,22 +1,11 @@
 require 'spec_helper'
 
-#describe "AuthenticationPages" do
-#  describe "GET /authentication_pages" do
-#    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get authentication_pages_index_path
-#      response.status.should be(200)
-#    end
-#  end
-#end
-
 describe "Authentication" do
 
   subject { page }
 
   describe "signin page" do
     before { visit signin_path }
-
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end  # describe "signin page" do
@@ -27,9 +16,7 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
       it { should have_title('Sign in') }
-#      it { should have_selector('div.alert-error', text: 'Invalid') }	#chap 8-5-2でコメントアウト
-       it { should have_error_message('Invalid') }
-
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -39,11 +26,6 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-#      before do	#chap 8-5-2でコメントアウト	
-#        fill_in "Email", with: user.email.upcase
-#        fill_in "Password", with: user.password
-#        click_button "Sign in"
-#      end  # before do
       before { valid_signin(user) }	#chap9-1-1
       before { sign_in user }
 
@@ -59,4 +41,57 @@ describe "Authentication" do
       it { should_not have_link('Sign in', href: signin_path) }
     end  # describe "with valid information" do
   end  # describe "signin" do
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protested page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end  # before do
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end  # it "should render ~ do
+        end  # describe "after signing in" do
+      end  # describe "when attempting ~ do
+
+      describe "in the Users controller" do
+      	
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end  # describe "submitting to the update action" do
+      end
+    end  # describe "for non-signed-in users" do
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end  # describe "submitting a GET request ~
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end  # describe "submitting a PATCH request ~
+    end  # describe "as wrong user" do
+  end
 end  # describe "Authentication" do
